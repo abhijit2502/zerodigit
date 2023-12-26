07bcf362f68634ef4b4b07e6bc36a66589f1288a
Abhijit Rokade <abhijeetrokade25@gmail.com>
2023-12-26 16:26:08 +0530

    first commit

---
+++ src/app/services/http.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  baseUrl:string="http://localhost:3000/";
+  
+
+
+
+  constructor(private http:HttpClient) { }
+
+getDataFromServer(endpoint:string){
+  const url=this.baseUrl+endpoint;
+  return this.http.get(url);
+}
+
+
+
+}
